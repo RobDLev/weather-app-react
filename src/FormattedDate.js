a2d96c62a42760fa3bafa5fc4f2b9700983b79de
RobDLev <75288391+RobDLev@users.noreply.github.com>
2021-04-14 20:40:14 -0700

    Added to FormattedDate component

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -2,12 +2,46 @@ import React from "react";
 import "./FormattedDate.css";
 
 export default function FormattedDate() {
+  let now = new Date();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  let year = now.getFullYear();
+
   return (
     <div className="FormattedDate">
       <div id="date-and-time">
-        Sunday, 10:52
+        {day}, {hours}:{minutes}
         <br />
-        February 28, 2021
+        {month} {date}, {year}
       </div>
     </div>
   );
